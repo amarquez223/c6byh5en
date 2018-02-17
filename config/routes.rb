a6ca1343d78afcf79ba8e6b9f62b959e9fd64f5d
anderson <anderson@debian.amrconsulting.com>
2018-02-16 19:22:59 -0500

    Adición de controlador basado en API con formato JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
   #Definición de la API
-  namespace :api, defaults: {format: :json},
-                              constraints: {subdomain: 'api'}, path:  '/' do
+  namespace :api, defaults: {format: :json} do
 
+    namespace :v1 do
+      get 'products', to: "products#index"
+    end
+    
   end
 
 end
